f46f55b9e379577fa7bb52a5e0ef6e4d546aaa9e
theToxicKnight <michaelomordia.mo@gmail.com>
2021-04-17 19:45:10 -0500

    final

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,16 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net', 
+    :port => '587', 
+    :authentication => :plain, 
+    :user_name => ENV['SENDGRID_USERNAME'], 
+    :password => ENV['SENDGRID_PASSWORD'], 
+    :domain => 'heroku.com', 
+    :enable_starttls_auto => true 
+  }
+
   config.action_mailer.delivery_method = :test 
   config.action_mailer.default_url_options = { :host => 'http://ruby-on-rails-123170.nitrousapp.com:3000'}
   # Show full error reports.
